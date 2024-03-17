75658b45a79a201ce2f6557708738ca3592e0ad1
Arda Ungun <arda@ungun.me>
2024-03-16 23:56:37 -0400

    app/page.tsx: commenting in MessageCreateForm causes 2 errors: `i.set` and `e._formData.get` is not a function

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { MessageList } from "@/app/ui/components/MessageList";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center gap-12 p-4">
-      {/* <MessageCreateForm /> */}
+      <MessageCreateForm />
       <MessageList />
     </main>
   );
