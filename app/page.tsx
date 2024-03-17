304d1d68f224d3b2bc1a94cc146ab7aceba487e8
Arda Ungun <arda@ungun.me>
2024-03-16 23:28:10 -0400

    app/page: top-align messages, comment out messageCreateForm

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import { MessageList } from "@/app/ui/components/MessageList";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-4">
-      <MessageCreateForm />
+    <main className="flex min-h-screen flex-col items-center gap-12 p-4">
+      {/* <MessageCreateForm /> */}
       <MessageList />
     </main>
   );
