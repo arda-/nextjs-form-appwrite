9a9a6f3dfd8d1d4241aaea2f532ae977c097f564
Arda Ungun <arda@ungun.me>
2024-03-16 23:26:42 -0400

    install appwrite, install latest react, react-dom, typescript

--- app/actions.ts
+++ app/actions.ts
@@ -5,73 +5,71 @@ import { z } from "zod";
 import { fromErrorToFromState } from "./utils";
 import type { FormState } from "./utils";
 
+import { databases, DATABASE_ID, COLLECTION_IDS } from "./lib/appwrite";
+import { ID, Models } from "appwrite";
+
 type Message = {
-  id: string;
   text: string;
 };
 
-/*export type FormState = {
-    message: string;
-  };*/
+let messages = [];
 
-
-let messages: Message[] = [
-  {
-    id: crypto.randomUUID(),
-    text: "First Message",
-  },
-  {
-    id: crypto.randomUUID(),
-    text: "Second Message",
-  },
-  {
-    id: crypto.randomUUID(),
-    text: "Third Message",
-  },
-];
-
-export const getMessages = async (): Promise<Message[]> => {
-  await new Promise((resolve) => setTimeout(resolve, 250));
-
-  return Promise.resolve(messages);
+export const getMessages = async (): Promise<Models.Document[]> => {
+  try {
+    const dbResponse = await databases.listDocuments(
+      DATABASE_ID,
+      COLLECTION_IDS.MESSAGES,
+      []
+    );
+    // console.log(
+    //   "fetched view settings",
+    //   JSON.stringify(dbResponse.documents, null, 2)
+    // );
+    return dbResponse.documents;
+  } catch (e) {
+    console.error(e);
+    return [];
+  }
 };
 
 const createMessageSchema = z.object({
-    text: z.string().min(1).max(191),
-    title: z.string().min(1).max(191),
+  text: z.string().min(1).max(200),
 });
 
-export const createMessage = async (formState: FormState, formData: FormData) => {
-  await new Promise((resolve) => setTimeout(resolve, 350));
-
+export const createMessage = async (
+  formState: FormState,
+  formData: FormData
+) => {
   try {
     const data = createMessageSchema.parse({
-        text: formData.get("text"),
-        title: formData.get("title"),
-      });
-    
-      messages.push({
-        id: crypto.randomUUID(),
-        ...data,
-      });
-      // throw new Error('Database error'); for error simulation
+      text: formData.get("text"),
+    });
+
+    const newDoc = await databases.createDocument(
+      DATABASE_ID,
+      COLLECTION_IDS.MESSAGES,
+      ID.unique(),
+      data
+    );
+
+    // throw new Error('Database error'); for error simulation
   } catch (error) {
     return fromErrorToFromState(error);
   }
 
   revalidatePath("/");
 
-  return toFormState('SUCCESS', 'Message created');
+  return toFormState("SUCCESS", "Message created");
 };
 
 export const toFormState = (
-    status: FormState['status'],
-    message: string
-  ): FormState => {
-    return {
-      status,
-      message,
-      fieldErrors: {},
-      timestamp: Date.now(),
-    };
+  status: FormState["status"],
+  message: string
+): FormState => {
+  return {
+    status,
+    message,
+    fieldErrors: {},
+    timestamp: Date.now(),
   };
+};
